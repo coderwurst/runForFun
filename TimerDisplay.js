3c7b4a3d00c2210a8748be54ba6d23d1a1752962
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-11 21:48:13 +0200

    add native timer

--- TimerDisplay.js
+++ TimerDisplay.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CountDown from 'react-native-countdown-component';
 import { Text, View } from 'react-native';
 
 class TimerDisplay extends Component {
@@ -6,7 +7,7 @@ class TimerDisplay extends Component {
     constructor(props) {
         super(props);
     }
-
+    
     runTraining() {
         // TODO 3 second countdown to start
 
@@ -16,19 +17,45 @@ class TimerDisplay extends Component {
 
         // TODO active time -> rest time etc.
     }
-    
+
     render() {
         const { navigation } = this.props;
-        const duration = navigation.getParam('duration', 'no duration');
-        const active = navigation.getParam('active', 'no active');
-        const rest = navigation.getParam('rest', 'no rest');
+        const duration = navigation.getParam('duration', '00');
+        const active = navigation.getParam('active', '00');
+        const rest = navigation.getParam('rest', '00');
 
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-                <Text>Hello from the Timer</Text>
-                <Text>Duration: { JSON.stringify(duration) }</Text>
-                <Text>Active: { JSON.stringify(active) }</Text>
-                <Text>Rest: { JSON.stringify(rest) }</Text>
+                <Text>Total Training Time</Text>
+                <CountDown
+                    id={ 'duration' }
+                    until={ Number(duration) }
+                    onFinish={ () => alert('finished') }
+                    onPress={ () => alert('hello') }
+                    size={ 20 }
+                    timeToShow={ ['M','S'] }
+                    />
+                <Text>Active Time</Text>
+                <CountDown
+                    id={ 'active' }
+                    ref= { 'activeTimer' } 
+                    until={ Number(active) }
+                    onFinish={ alert('finished') }
+                    onPress={ () => alert('hello') }
+                    size={ 20 }
+                    timeToShow={ ['M','S'] }
+                    />
+                <Text>Rest Time</Text>
+                <CountDown
+                    id={ 'rest' }
+                    ref={ 'restTimer' }
+                    until={ Number(rest) }
+                    onFinish={ alert('finished') }
+                    onPress={ () => alert('hello') }
+                    size={ 20 }
+                    timeToShow={ ['M','S'] }
+                    running={ false }
+                    />
             </View>
         )
     }
