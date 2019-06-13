7c7692d0e155d9fd5b5a06a935f794200dc1d765
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-13 21:26:44 +0200

    nav links for active and rest

--- TimerDisplay.js
+++ TimerDisplay.js
@@ -5,28 +5,47 @@ class TimerDisplay extends Component {
     
     constructor(props) {
         super(props);
+        const { navigation } = this.props;
+
+        this.state = { 
+            totalTraining: navigation.getParam('duration', '00'),
+            activeTime: navigation.getParam('active', '00'),
+            restTime: navigation.getParam('rest', '00')
+        };
     }
     
     runTraining() {
-        // TODO 3 second countdown to start
+        // TODO 3 second countdown to start & update up
 
-        // TODO show 1st active time
-
-        // TODO show 1st rest time
 
         // TODO active time -> rest time until duration has been met
+        for(i = 0; i < this.state.totalTraining; i++) {
+            // TODO show 1st active time (nav to Active.js)
+            this.loadActive(this.state.activeTime);
+            // TODO show 1st rest time (nav to Rest.js)
+            //this.loadRest(this.state.restTime);
+        }
     }
 
-    render() {
-        const { navigation } = this.props;
-        const duration = navigation.getParam('duration', '00');
-        const active = navigation.getParam('active', '00');
-        const rest = navigation.getParam('rest', '00');
+    loadActive(active){
+        this.props.navigation.navigate('ActiveDisplay', { 
+            active: active
+        });
+    }
+
+    loadRest(rest){
+        this.props.navigation.navigate('RestDisplay', { 
+            rest: rest
+        });
+    }
+
+    render() {    
+        this.runTraining();
 
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
                 <Text>Get Ready</Text>
-                <Text>{ duration } countdown </Text>
+                <Text>{ this.state.totalTraining } countdown </Text>
             </View>
         )
     }
