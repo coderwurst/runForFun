6db1aa2899435c47a0f2416ccdc121b7b9f76b99
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-14 21:27:13 +0200

    add icons for active and rest periods

--- ActivityDisplay.js
+++ ActivityDisplay.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, Vibration, View } from 'react-native';
+import { Image, Text, Vibration, View } from 'react-native';
 
 class ActivityDisplay extends Component {
     
@@ -60,12 +60,15 @@ class ActivityDisplay extends Component {
         }
     }
 
-    render() {    
+    render() {   
+        let icon = this.state.active ? 
+                    require('./fitness.png') : 
+                    require('./battery-charging.png')  
         return (
             <View style={ this.state.active ? ({ flex: 1, alignItems: 'center', justifyContent: 'center',
             backgroundColor: '#BDFF57' }) : ({ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#1D4ECC'})} >
-                <Text>{this.state.active ? 'Active' : 'Rest'}</Text>
-                <Text>Icon</Text>
+                <Text style={{ marginBottom: 24 }}>{this.state.active ? 'Active' : 'Rest'}</Text>
+                <Image source={icon} style={{ width: 256, height: 256, resizeMode: 'contain' }}/>
             </View>
         )
     }
